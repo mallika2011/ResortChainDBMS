a143ff14adf666b7ee2653ce1f8834d6aaf706ac
Mallika <mallika2011@yahoo.com>
2019-11-12 00:52:31 +0530

    All Creates

--- resortChain.py
+++ resortChain.py
@@ -59,7 +59,7 @@ def addMember():
     row["Email_ID"] = input("Email ID: ")
 
     row["Package"] = "Red"  # By default we are giving red
-    row['Monthly_Fee']=500
+    row['Monthly_Fee'] = 500
     row_add = []
     n = int(input("Enter the number of member's addresses you want to input: "))
     for i in range(n):
@@ -104,15 +104,29 @@ def addMemberGuest():
     row["Check-Out_Date"] = input("Check-Out_Date(YYYY-MM-DD): ")
 
     # Not doing anything for cost of staying
-    query = "INSERT INTO MEMBER_GUESTS(Hotel_ID, Room_No, Member_ID, Check_In_Date, Check_Out_Date,Cost_Of_Staying) VALUES('%s', '%s', '%s', '%s', '%s',0 )" % (
-        row["Hotel_ID"], row["Room_Number"], row["Member_ID"], row["Check-In_Date"], row["Check-Out_Date"])
+
+    query = "SELECT isOccupied FROM HOTEL_I WHERE HOTEL_ID=%s AND ROOM_NUMBER=%s" % (
+        row["Hotel_ID"], row["Room_Number"])
     cur.execute(query)
+    x = cur.fetchone()
     con.commit()
-       
-    query= "IF(SELECT isOccupied FROM HOTEL_I WHERE ROOM_NUMBER=%s AND HOTEL_ID=%s, UPDATE MEMBER_GUESTS M SET COST_OF_STAYING = (CHECK_OUT_DATE-CHECK_IN_DATE)*(SELECT PRICE_DAY FROM HOTEL_I WHERE HOTEL_ID = M.HOTEL_ID AND M.ROOM_NO=ROOM_NUMBER),) " %(
-    row["Room_Number"], row["Hotel_ID"])
-    cur.execute(query)
-    con.commit()
+
+    if x["isOccupied"] == 0:
+        query = "INSERT INTO MEMBER_GUESTS(Hotel_ID, Room_No, Member_ID, Check_In_Date, Check_Out_Date,Cost_Of_Staying) VALUES('%s', '%s', '%s', '%s', '%s',0 )" % (
+            row["Hotel_ID"], row["Room_Number"], row["Member_ID"], row["Check-In_Date"], row["Check-Out_Date"])
+        cur.execute(query)
+        con.commit()
+
+        query = "UPDATE MEMBER_GUESTS M SET COST_OF_STAYING = (CHECK_OUT_DATE-CHECK_IN_DATE)*(SELECT PRICE_DAY FROM HOTEL_I WHERE HOTEL_ID = M.HOTEL_ID AND M.ROOM_NO=ROOM_NUMBER)"
+        cur.execute(query)
+        con.commit()
+
+        query = "UPDATE HOTEL_I SET ISOCCUPIED=1,ISMEMBER=1 WHERE HOTEL_ID=%s AND ROOM_NUMBER=%s" % (
+            row["Hotel_ID"], row["Room_Number"])
+        cur.execute(query)
+        con.commit()
+    else:
+        print("Room already occupied")
 
     return
 
@@ -122,22 +136,36 @@ def addNonMemberGuest():
     row = {}
     print("Enter the non-member guest details: ")
     row["Hotel_ID"] = input("Hotel_ID: ")
-    row["Room_Number"] = input("Room_Number")
-    name = (input("Name (First_Name Last_Name): ")).split(' ')
-    row["First_Name"] = name[0]
-    row["Last_Name"] = name[1]
-    row["Check-In_Date"] = input("Check-In_Date(DD/MM/YYYY): ")
-    row["Check-Out_Date"] = input("Check-Out_Date(DD/MM/YYYY): ")
-    row["Phone_Number"] = input("Phone_Number")
+    row["Room_Number"] = input("Room_Number: ")
+    row["First_Name"] = input("First Name: ")
+    row["Last_Name"] = input("Last Name: ")
+    row["Check-In_Date"] = input("Check-In_Date(YYYY-MM-DD): ")
+    row["Check-Out_Date"] = input("Check-Out_Date(YYYY-MM-DD): ")
+    row["Phone_Number"] = input("Phone Number: ")
 
-    query = "INSERT INTO NON-MEMBER_GUESTS(Hotel_ID, Room_No, First_Name, Last_Name, Check_In_Date, Check_Out_Date, Phone_Number) VALUES('%s', '%s', '%s', '%s', '%s', '%s', '%s')" % (
-        row["Hotel_ID"], row["Room_Number"], row["First_Name"], row["Last_Name"], row["Check-In_Date"], row["Check-Out_Date"], row["Phone_Number"])
+    query = "SELECT isOccupied FROM HOTEL_I WHERE HOTEL_ID=%s AND ROOM_NUMBER=%s" % (
+        row["Hotel_ID"], row["Room_Number"])
     cur.execute(query)
+    x = cur.fetchone()
+    # print(x)
     con.commit()
-    
-    query= "UPDATE NON_MEMBER_GUESTS M SET COST_OF_STAYING = (CHECK_OUT_DATE-CHECK_IN_DATE)*(SELECT PRICE_DAY FROM HOTEL_I WHERE HOTEL_ID = M.HOTEL_ID AND M.ROOM_NO=ROOM_NUMBER);"
-    cur.execute(query)
-    con.commit()
+
+    if x["isOccupied"] == 0:
+        query = "INSERT INTO NON_MEMBER_GUESTS(Hotel_ID, Room_No, First_Name, Last_Name, Check_In_Date, Check_Out_Date, Phone_Number,Cost_Of_Staying) VALUES('%s', '%s', '%s', '%s', '%s', '%s', '%s',0)" % (
+            row["Hotel_ID"], row["Room_Number"], row["First_Name"], row["Last_Name"], row["Check-In_Date"], row["Check-Out_Date"], row["Phone_Number"])
+        cur.execute(query)
+        con.commit()
+
+        query = "UPDATE NON_MEMBER_GUESTS M SET COST_OF_STAYING = (CHECK_OUT_DATE-CHECK_IN_DATE)*((SELECT PRICE_DAY FROM HOTEL_I WHERE HOTEL_ID = M.HOTEL_ID AND M.ROOM_NO=ROOM_NUMBER)+1000);"
+        cur.execute(query)
+        con.commit()
+
+        query = "UPDATE HOTEL_I SET ISOCCUPIED=1,ISMEMBER=0 WHERE HOTEL_ID=%s AND ROOM_NUMBER=%s" % (
+            row["Hotel_ID"], row["Room_Number"])
+        cur.execute(query)
+        con.commit()
+    else:
+        print("Room already occupied")
 
     return
 
@@ -155,19 +183,98 @@ def addPackage():
     return
 
 
+def addFinance():
+    global cur
+    row = {}
+    print("Enter Finance Details: ")
+
+    row["Hotel_ID"] = input("Hotel_ID: ")
+    row["Month"] = input("Month: ")
+    row["Income"] = int(input("Income: "))
+    row["Expenditure"] = int(input("Expenditure: "))
+
+    query = "INSERT INTO FINANCE(Hotel_ID, Expenditure, Income, Month, Profit) VALUES('%s', '%d', '%d', '%s',0)" % (
+        row["Hotel_ID"], row["Expenditure"], row["Income"], row["Month"])
+    cur.execute(query)
+    con.commit()
+
+    query = "UPDATE FINANCE F SET PROFIT = (INCOME-EXPENDITURE) WHERE HOTEL_ID ='%s' AND MONTH='%s'" % (
+        row["Hotel_ID"], row["Month"])
+    cur.execute(query)
+    con.commit()
+
+    return
+
+
+def addServiceProvider():
+    global cur
+    row = {}
+    print("Enter Service Provider Details: ")
+
+    row["Service_Provider"] = input("Service_Provider: ")
+    row["Service_Description"] = input("Service_Description: ")
+    row["Service_Price"] = int(input("Service_Price: "))
+
+    query = "INSERT INTO PROVIDERS_SERVICES(Service_Provider, Service_Description, Service_Price) VALUES('%s', '%s', '%d')" % (
+        row["Service_Provider"], row["Service_Description"], row["Service_Price"])
+    cur.execute(query)
+    con.commit()
+    return
+
+
+def addRoom():
+    global cur
+    row = {}
+    print("Enter the new room's details: ")
+
+    row["Room_Type"] = input("Room_Type: ")
+    row["Price_Day"] = input("Price Per Day: ")
+    row["Room_Number"] = input("Room_Number: ")
+    row["Hotel_ID"] = input("Hotel ID: ")
+
+    query = "INSERT INTO HOTEL_I(isMember,isOccupied,Room_Type,Price_Day,Room_Number,Hotel_ID) VALUES(NULL,0,'%s', '%s', '%s', '%s')" % (
+        row["Room_Type"], row["Price_Day"], row["Room_Number"], row["Hotel_ID"])
+    cur.execute(query)
+    con.commit()
+    return
+
+
+def addHotel():
+    global cur
+    row = {}
+    print("Enter the new hotel's details: ")
+    row["Hotel_ID"] = input("Hotel ID: ")
+    row["Hotel_Name"] = input("Hotel_Name: ")
+    row["Location"] = input("Location: ")
+    row["Manager_ID"] = input("Manager_ID: ")
+
+    query = "INSERT INTO DESTINATION(Hotel_ID,Hotel_Name,Location,Manager_ID) VALUES('%s', '%s', '%s', '%s')" % (
+        row["Hotel_ID"], row["Hotel_Name"], row["Location"], row["Manager_ID"])
+    cur.execute(query)
+    con.commit()
+
+    query = "UPDATE EMPLOYEE SET HOTEL_ID='%s' WHERE EMPLOYEE_ID=%s" % (
+        row["Hotel_ID"], row["Manager_ID"])
+    cur.execute(query)
+    con.commit()
+
+    return
+
+
 optionFunctionMapping = {
     1: addEmployee,
     2: addMember,
     3: addMemberGuest,
     4: addNonMemberGuest,
-    5: addPackage
-    # 5: addFinance,
-    # 6: addServiceProvider,
-    # 7: addRoom
+    5: addPackage,
+    6: addFinance,
+    7: addServiceProvider,
+    8: addRoom,
+    9: addHotel
 }
 
 while(1):
-    tmp = sp.call('clear', shell=True)
+    # tmp = sp.call('clear', shell=True)
     # username = input("Username: ")
     # password = input("Password: ")
 
@@ -193,7 +300,10 @@ while(1):
         #         send(optionFunctionMapping[c]())
         # addEmployee()
         # addMember()
-        addMemberGuest()
+        # addNonMemberGuest()
+        # addServiceProvider()
+        # addFinance()
+        addRoom()
 
     # except:
     #     #tmp = sp.call('clear', shell=True)
